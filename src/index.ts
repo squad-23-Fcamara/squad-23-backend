5f295ed2f73f40284e1f297f4ba5a444c81105e3
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-05 23:30:35 -0300

    typescript configuration and create home route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
+import { homeRoute } from './routes/home.route'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(homeRoute)
 
 const PORT = process.env.PORT || 5000
 
---
+++ src/routes/home.route.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express'
+
+const homeRoute = Router()
+
+homeRoute.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Squad23 API')
+})
+
+export { homeRoute }

f82d62995f6c3b4484e66901c43716f8cf36b5ff
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-08 15:44:56 -0300

    Updated ScheduleMentoringSessionService to handle with the new params of the mentoring

--- src/services/ScheduleMentoringSessionService/index.ts
+++ src/services/ScheduleMentoringSessionService/index.ts
@@ -4,17 +4,30 @@ interface IMentoringSessionProps {
   schedule_to: Date;
   mentor_id: string;
   mentored_id: string;
+  notes?: string;
+  theme: string[];
+  platform: string;
 }
 
 class ScheduleMentoringSessionService {
-  async execute({ mentor_id, mentored_id, schedule_to }: IMentoringSessionProps) {
+  async execute({ 
+    mentor_id,
+    mentored_id,
+    schedule_to,
+    notes,
+    theme,
+    platform 
+  }: IMentoringSessionProps) {
     try {
-      const mentoringSession = await prismaClient.public_schedules.create({
+      const mentoringSession = await prismaClient.public_mentorings.create({
         data: {
           schedule_to,
           public_users: {
             connect: [{ id: mentor_id }, { id: mentored_id }]
-          }
+          },
+          notes,
+          theme,
+          platform
         }
       })
 

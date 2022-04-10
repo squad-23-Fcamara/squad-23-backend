899dab3cdb0188379f6be8c30a1762e588be909c
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-10 20:37:33 -0300

    Refactored getbyidservice

--- src/services/GetByIdUserService/index.ts
+++ src/services/GetByIdUserService/index.ts
@@ -1,17 +1,19 @@
 import { GetByIdUserRepository } from '../../repository/GetByIdUserRepository'
-import { CreateUserRepository } from '../../repository/CreateUserRepository'
+
 class GetByIdUserService {
   async execute(id: string) {
     const getByIdUserRepository =  new GetByIdUserRepository()
-    const userIdDontExists = new CreateUserRepository()
+    try {
+      const user = await getByIdUserRepository.findUnique(id)
 
-    if(userIdDontExists) {
-      throw new Error ("This user doesn't exist, create a new user")
+      if(!user){
+        throw new Error ("This user doesn't exist")
+      }
+
+      return user
+    } catch (error) {
+      return error 
     }
-
-    const user = await getByIdUserRepository.findUnique(id)
-
-    return user
   }
 }
 

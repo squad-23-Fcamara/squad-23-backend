012a6e36718e377db61df01266c6e7a9cc71ff55
JosanaKla <josana.0205@gmail.com>
2022-04-10 10:21:24 -0300

    changes to create service

--- src/services/CreateUserService/index.ts
+++ src/services/CreateUserService/index.ts
@@ -1,10 +1,19 @@
-import { prismaClient } from '../../utils/prisma'
 import { IUserProps } from '../../@types/Interfaces/IUserProps'
-import { CreateUserRepository } '../../repository/CreateUserRepository'
+import { CreateUserRepository } from '../../repository/CreateUserRepository'
 class CreateUserService {
-  async execute() {
+  async execute({ 
+    name, 
+    email, 
+    role, 
+    password 
+  }: IUserProps) {
+    const userDontExists = new CreateUserRepository()
+    const userAlreadyExists = 
+
     try {
-      })
+      if(userDontExists) {
+        
+      }
       return 
     } catch (error: any) {
       throw { ...error, message: "Email already exists"}

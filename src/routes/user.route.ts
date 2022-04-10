fcc3bde0c4f442d322fd872165037cab16643efd
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-10 18:59:24 -0300

    created route that returns mentors by skills

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import { CreateUserController } from '../controllers/CreateUserController'
-import { GetAllUserController } from '../controllers/GetAllUserController'
+import { GetMentorsBySkillsController } from '../controllers/GetMentorsBySkillsController'
 import { GetByIdUserController } from '../controllers/GetByIdUserController'
 import { UpdateUserController } from '../controllers/UpdateUserController'
 
 const userRoute = Router()
 
 userRoute.post('/users/create', new CreateUserController().handle)
-userRoute.get('/users', new GetAllUserController().handle)
+userRoute.get('/mentors/:skills', new GetMentorsBySkillsController().handle)
 userRoute.get('/users/:uuid', new GetByIdUserController().handle)
 userRoute.put('/users/:uuid', new UpdateUserController().handle)
 

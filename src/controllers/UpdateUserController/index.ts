6a7d3f5dfbc6de7317e9828dc9ac0ec6d3ed046b
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-11 16:33:33 -0300

    Updated controller to update user route

--- src/controllers/UpdateUserController/index.ts
+++ src/controllers/UpdateUserController/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { IUserProps } from '../../@types/Interfaces/IUserProps'
 import { UpdateUserService } from '../../services/UpdateUserService'
 
 class UpdateUserController {
@@ -6,10 +7,28 @@ class UpdateUserController {
 
     const service = new UpdateUserService()
     const { uuid } = req.params
-    const { mentor, skills } = req.body
+    const { ...props }: IUserProps = req.body 
 
     try {
-      const user = await service.execute(uuid, mentor, skills)
+      if (Object.values(props).some((item) => item === null)){
+        // Valida se recebemos algum dado como null, não é permitido
+        res.status(400)
+        throw new Error(`You have passed null propertys and it's not possible`)
+      }
+
+      if (Object.values(props).every((item) => item === undefined)){
+        // Valida se o body da requisição veio vazio, não é permitido
+        res.status(400)
+        throw new Error(`Missing body of request`)
+      }
+
+      if (Object.values(props).some((item) => item.length === 0)){
+        // Impede que strings e arrays vazios sejam enviadas ao banco de dados
+        res.status(400)
+        throw new Error(`Invalid param on request`)
+      }
+
+      const user = await service.execute(uuid, props)
 
       res.json(user)
     } catch (error: any) {

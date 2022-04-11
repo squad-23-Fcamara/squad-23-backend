df9855e48d775102b06b2fad78fcaa34a23e1658
JosanaKla <josana.0205@gmail.com>
2022-04-11 12:09:40 -0300

    initialized swagger config

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,8 @@ import { CreateUserController } from '../controllers/CreateUserController'
 import { GetMentorsBySkillsController } from '../controllers/GetMentorsBySkillsController'
 import { GetByIdUserController } from '../controllers/GetByIdUserController'
 import { UpdateUserController } from '../controllers/UpdateUserController'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from '../swagger.json'
 
 const userRoute = Router()
 
@@ -10,6 +12,6 @@ userRoute.post('/users/create', new CreateUserController().handle)
 userRoute.get('/mentors/:skills', new GetMentorsBySkillsController().handle)
 userRoute.get('/users/:uuid', new GetByIdUserController().handle)
 userRoute.put('/users/:uuid', new UpdateUserController().handle)
-
+userRoute.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export { userRoute }

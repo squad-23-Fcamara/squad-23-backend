a4ca3354381d17eb110524bc59cc3b8ac27232be
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-11 17:41:03 -0300

    Created route to cancel mentorings and controller to this route

--- src/routes/schedule.route.ts
+++ src/routes/schedule.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { DeleteMentoringController } from '../controllers/DeleteMentoringController'
 import { ScheduleMentoringSessionController } from '../controllers/ScheduleMentoringSessionController'
 
 const scheduleRoutes = Router()
 
 scheduleRoutes.post('/schedule/create', new ScheduleMentoringSessionController().handle)
+scheduleRoutes.delete('/schedule/delete/:uuid', new DeleteMentoringController().handle)
 
 export { scheduleRoutes }

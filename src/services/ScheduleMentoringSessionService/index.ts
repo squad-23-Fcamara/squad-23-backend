1124dd499b55d39618cca656594b592153a131a5
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-12 16:45:52 -0300

    Refactored code and added a validation that don't allow user to schedule more than 2 mentorings in a single day

--- src/services/ScheduleMentoringSessionService/index.ts
+++ src/services/ScheduleMentoringSessionService/index.ts
@@ -1,5 +1,6 @@
 import { VerifyUserMentoringExistisRepository } from '../../repository/VerifyUserMentoringExistisRepository'
 import { ScheduleMentoringRepository } from '../../repository/ScheduleMentoringRepository'
+import { CheckMentoringsRepository } from '../../repository/CheckMentoringsRepository'
 import { IMentoringSessionProps } from '../../@types/Interfaces/IMentoringSessionProps'
 
 class ScheduleMentoringSessionService {
@@ -13,32 +14,37 @@ class ScheduleMentoringSessionService {
   }: IMentoringSessionProps) {
     const verifyUserMentoringExistisRepository = new VerifyUserMentoringExistisRepository()
     const scheduleMentoringRepository = new ScheduleMentoringRepository()
+    const checkMentoringsRepository = new CheckMentoringsRepository()
 
     const mentorMentorings = await verifyUserMentoringExistisRepository.verify(mentor_id, new Date(schedule_to).toISOString())
     const mentoredMentorings = await verifyUserMentoringExistisRepository.verify(mentored_id, new Date(schedule_to).toISOString())
+    const verifyMentorings = await checkMentoringsRepository.check(mentored_id)
 
-    try {
-      if (mentorMentorings) {
-        throw new Error('Mentor already have mentoring in this date')
-      }
-
-      if (mentoredMentorings) {
-        throw new Error('Mentored already have mentoring in this date')
-      }
-
-      const mentoringSession = await scheduleMentoringRepository.schedule({
-          schedule_to,
-          mentor_id,
-          mentored_id,
-          notes,
-          theme,
-          platform
-        })
-
-      return mentoringSession
-    } catch (error: any) {
-      return error.message
+    const numberOfMentorings = verifyMentorings?.mentorings.filter(item =>
+        item.schedule_to.toLocaleDateString() === new Date(schedule_to).toLocaleDateString())
+    
+    if (numberOfMentorings!.length >= 2){
+      throw new Error('Mentored already have limit of mentorings for this day')
     }
+
+    if (mentorMentorings) {
+      throw new Error('Mentor already have mentoring in this date')
+    }
+
+    if (mentoredMentorings) {
+      throw new Error('Mentored already have mentoring in this date')
+    }
+
+    const mentoringSession = await scheduleMentoringRepository.schedule({
+        schedule_to,
+        mentor_id,
+        mentored_id,
+        notes,
+        theme,
+        platform
+    })
+
+    return mentoringSession
   }
 }
 

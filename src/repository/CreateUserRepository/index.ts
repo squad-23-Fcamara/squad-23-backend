7aaabe8bc14445c8160dbe5e0d88b8c200db8f52
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-13 10:34:58 -0300

    Fixed create user repository warning and refactored some code

--- src/repository/CreateUserRepository/index.ts
+++ src/repository/CreateUserRepository/index.ts
@@ -1,14 +1,20 @@
 import { prismaClient } from "../../utils/prisma";
-import { IUserProps } from '../../@types/Interfaces/IUserProps'
 
 class CreateUserRepository {
-  async create(user : IUserProps) {
-    const createUser = await prismaClient.public_users.create({
-      data: {
-      ...user
-      }
-    })
-    return createUser;
+  async create(name: string, email: string, role: string, password: string) {
+    try {
+      const createUser = await prismaClient.public_users.create({
+        data: {
+          name,
+          email,
+          role,
+          password
+        }
+      })
+      return createUser;
+    } catch (error) {
+      throw error
+    }
   }
 }
 
